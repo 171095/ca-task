1887751a7b1269d121e742861dcd8a0fca3ad203
nikita_pandey <pandeynikita123@gmail.com>
2018-02-27 16:36:36 +0530

    Assigned roles

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,5 +2,24 @@ class Staff < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  # Define Roles
+
+  def audit?
+    self.role == "audit"
+  end
+
+  def article?
+    self.role == "article"
+  end
+
+  def manager?
+    self.role == "manager"
+  end
+
+  def ca?
+    self.role == "ca"
+  end
+
 end
